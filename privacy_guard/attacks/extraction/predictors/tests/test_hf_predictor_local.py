ca0fdfb91b0b9e98b300856fc2834dbb3fc54330
Matthew Grange <mgrange@meta.com>
2025-09-22 06:11:24 -0700

    Update PrivacyGuard Attack License Headers to MIT (#57)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/PrivacyGuard/pull/57
    
    This updates the Attack OSS files of PrivacyGuard to reflect the MIT license
    
    Reviewed By: lucamelis, iden-kalemaj
    
    Differential Revision: D82836646
    
    fbshipit-source-id: 3303d42899974a996156984d1c8c1935154f9b0b

--- privacy_guard/attacks/extraction/predictors/tests/test_hf_predictor_local.py
+++ privacy_guard/attacks/extraction/predictors/tests/test_hf_predictor_local.py
@@ -1,6 +1,9 @@
-# pyre-strict
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+# pyre-strict
 
 """
 Test script for HuggingFacePredictor generation capabilities.
